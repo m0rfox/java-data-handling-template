78e6bbdb064fbac28d91066547772f1479c6fd69
Valerii Glavatskih <glavatskihvalerii@yandex.ru>
2021-06-28 20:54:24 +0400

    №5. Реализован класс SimpleFileRepository.

--- src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
@@ -1,5 +1,13 @@
 package com.epam.izh.rd.online.repository;
 
+import java.io.*;
+
+import java.net.URL;
+import java.nio.file.Files;
+
+import java.nio.file.Paths;
+
+
 public class SimpleFileRepository implements FileRepository {
 
     /**
@@ -10,8 +18,23 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public long countFilesInDirectory(String path) {
-        return 0;
+        long count = 0;
+
+
+        File[] files;
+        File file = new File(path);
+        files = file.listFiles();
+        if (files == null) return count;
+        for (File i : files) {
+            if (i.isFile()) {
+                count++;
+            } else if (i.isDirectory()) {
+                count = count + countFilesInDirectory(i.getAbsolutePath());
+            }
+        }
+        return count;
     }
+
 
     /**
      * Метод рекурсивно подсчитывает количество папок в директории, считая корень
@@ -21,7 +44,23 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public long countDirsInDirectory(String path) {
-        return 0;
+
+        long count = 0;
+        File[] files;
+        File file = new File(path);
+        files = file.listFiles();
+        if (files == null) return count;
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                count++;
+                count += countDirsInDirectory(files[i].getAbsolutePath());
+
+                if (i == files.length - 1){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
     /**
@@ -32,7 +71,24 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public void copyTXTFiles(String from, String to) {
-        return;
+        if (Files.exists(Paths.get(to))) {
+            System.out.println("Директория уже существует");
+        }
+        File file = new File(from);
+        FileFilter filter = new FileFilter() {
+            public boolean accept(File f) {
+                return f.getName().endsWith("txt");
+            }
+        };
+        File[] files = file.listFiles(filter);
+        assert files != null;
+        for (File value : files) {
+            try {
+                Files.copy(Paths.get(from + "/" + value.getName()), Paths.get(to + "/" + value.getName()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
@@ -44,7 +100,33 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public boolean createFile(String path, String name) {
-        return false;
+        path = "target/classes/" + path;
+        if (Files.exists(Paths.get(path))){
+            System.out.println("Директория уже существует");
+            try {
+                Files.createFile(Paths.get(path + "/" + name));
+                if (Files.exists(Paths.get(path + "/" + name))){
+                    System.out.println("Файл создан");
+                }
+            } catch (IOException e) {
+                System.out.println("Файл уже создан");
+            }return Files.exists(Paths.get(path + "/" + name));
+        }else {
+            try {
+                Files.createDirectory(Paths.get(path));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                Files.createFile(Paths.get(path + "/" + name));
+            } catch (IOException e) {
+                System.out.println("Файл уже создан");;
+            }
+        }
+        if (Files.exists(Paths.get(path  + "/" + name))){
+            System.out.println("Файл создан");
+        }
+        return Files.exists(Paths.get(path  + "/" + name));
     }
 
     /**
@@ -55,6 +137,13 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public String readFileFromResources(String fileName) {
-        return null;
+        String path = "src/main/resources/";
+        String text = null;
+        try (BufferedReader br = new BufferedReader(new FileReader(path + fileName))) {
+            text = br.readLine();
+        } catch (IOException e) {
+            System.out.println("Файл не найден");
+        }
+        return text;
     }
 }
--- src/test/java/com/epam/izh/rd/online/FileRepositoryTest.java
+++ src/test/java/com/epam/izh/rd/online/FileRepositoryTest.java
@@ -20,11 +20,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FileRepositoryTest {
 
-  private static final String TEST_DIR_COUNT_PATH = "testDirCountFiles";
+  private static final String TEST_DIR_COUNT_PATH = "src/main/resources/testDirCountFiles";
   private static final String TEST_DIR_CREATE_PATH = "testDirCreateFile";
   private static final String TEST_FILE_TO_CREATE = "newFile.txt";
-  private static final String SOURCE_FILE = "fileRepository/source/TestFileToCopy.txt";
-  private static final String COPY_FILE = "fileRepository/copy/TestFileToCopy.txt";
+  private static final String SOURCE_FILE = "fileRepository/source";
+  private static final String COPY_FILE = "fileRepository/copy";
 
   private static FileRepository fileRepository;
 
